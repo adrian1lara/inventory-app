9692211cbdf1741ccb04781c25d5c4ecda3f2928
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-11-14 00:34:04 -0500

    added: virtual url to link detail page

--- models/category.js
+++ models/category.js
@@ -5,8 +5,12 @@ const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
   name: { type: String, required: true },
   description: { type: String },
-  url: { type: String }
 });
+
+CategorySchema.virtual('url').get(function() {
+  return `/category/${this._id}`
+})
+
 
 module.exports = mongoose.model('Category', CategorySchema);
 
