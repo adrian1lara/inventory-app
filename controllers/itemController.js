bc3ef394e8241dfcf8560654adf85e67106e20c6
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-11-14 00:30:16 -0500

    added: item_detail func

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,7 @@
 const Items = require('../models/items');
 const Category = require('../models/category');
 const asyncHandler = require('express-async-handler');
+const mongoose = require('mongoose');
 
 
 // display list of items
@@ -12,3 +13,26 @@ exports.item_list = asyncHandler(async (req, res, next) => {
   });
 });
 
+
+// detail page for item selected
+exports.item_detail = asyncHandler(async (req, res, next) => {
+  try {
+    const isValidObjectId = mongoose.Types.ObjectId.isValid(req.params.id);
+    if (!isValidObjectId) {
+      return res.status(400).send('Invalid item ID');
+    }
+
+    const oneItem = await Items.findById(req.params.id).exec();
+
+    if (!oneCategory) {
+      return res.status(404).send('Item not found');
+    }
+
+    res.render('item_detail', {
+      title: oneItem.name,
+      item_detail: oneItem
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
