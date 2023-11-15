36179708527ce0e577c0bbc78c293c3590da7b30
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-11-14 23:20:21 -0500

    added: routes to get and post item and categories to create

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,21 @@ router.get('/categories', category_controller.category_list);
 // get one category 
 router.get('/category/:id', category_controller.category_detail);
 
+// get create a category
+router.get('/categories/create', category_controller.category_create_get);
+
+// post request for creating category
+router.post('/categories/create', category_controller.genre_create_post);
+
 // items routes 
 router.get('/items', items_controller.item_list);
+
+// get create item 
+router.get('/item/create', items_controller.item_create_get);
+
+// post request for creating item 
+router.post('/item/create', items_controller.item_create_post);
+
 
 // get one item 
 router.get('/item/:id', items_controller.item_detail);
