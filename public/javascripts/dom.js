0673f4cc14236bfb7461dc72315793dc30e12c3a
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-11-18 02:23:22 -0500

    new: toggle func for responsive nav

---
+++ public/javascripts/dom.js
@@ -0,0 +1,8 @@
+document.getElementById('mobile-menu-button').addEventListener('click', function() {
+
+  let mobileMenu = document.getElementById('mobile-menu');
+
+  mobileMenu.classList.toggle('hidden');
+
+});
+
